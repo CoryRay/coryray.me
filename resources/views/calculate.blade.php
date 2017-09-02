b325f214263206c7b7db32db878861c41fe7f02b
Cory Rodriguez <coreyreylp@gmail.com>
2017-09-02 17:26:45 -0500

    Unit price styles

--- resources/views/calculate.blade.php
+++ resources/views/calculate.blade.php
@@ -22,14 +22,26 @@
         main {
             margin-bottom: 2rem;
         }
-        footer {
-            text-align: center;
+        fieldset {
+            -webkit-box-flex: 1;
+                -ms-flex: 1;
+                    flex: 1;
         }
         label {
             display: block;
         }
         input:not(:last-child) {
             margin-bottom: 1rem;
+        }
+        footer {
+            text-align: center;
+        }
+        .flex {
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: flex;
+            -ms-flex-wrap: wrap;
+                flex-wrap: wrap;
         }
     </style>
 </head>
@@ -38,29 +50,27 @@
     <h1>Do it</h1>
     <p>Use this to compare unit prices of two products, &amp; it&rsquo;s all done <mark>client side</mark>!</p>
 
-    <form id='form'>
-        <fieldset>
-            <legend>Product 1</legend>
+    <form id="form">
+        <div class="flex">
+            <fieldset>
+                <legend>Product 1</legend>
 
-            <label for="units">Units in item (g, oz, mL)</label>
-            <input type="number" name="item1[units]" id="units" min="0" placeholder="1000" required>
+                <label for="units">Units in item (g, oz, mL)</label>
+                <input type="number" name="item1[units]" id="units" min="0" placeholder="1000" required>
 
-            <label for="price">Price</label>
-            <input type="number" name="item1[price]" id="price" min="0" placeholder="20.99" step="0.01" required>
-        </fieldset>
-        <fieldset>
-            <legend>Product 2</legend>
+                <label for="price">Price</label>
+                <input type="number" name="item1[price]" id="price" min="0" placeholder="20.99" step="0.01" required>
+            </fieldset>
+            <fieldset>
+                <legend>Product 2</legend>
 
-            <label for="units2">Units in item (g, oz, mL)</label>
-            <input type="number" name="item2[units]" id="units2" min="0" placeholder="1000" required>
+                <label for="units2">Units in item (g, oz, mL)</label>
+                <input type="number" name="item2[units]" id="units2" min="0" placeholder="1000" required>
 
-            <label for="price2">Price</label>
-            <input type="number" name="item2[price]" id="price2" min="0" placeholder="20.00" step="0.01" required>
-        </fieldset>
-
-        {{-- <label for="tax">Include Texas sales tax?</label>
-        <input type="checkbox" name="tax" id="tax"> --}}
-
+                <label for="price2">Price</label>
+                <input type="number" name="item2[price]" id="price2" min="0" placeholder="20.00" step="0.01" required>
+            </fieldset>
+        </div>
         <button>Compute</button>
     </form>
     <div id="result"></div>
