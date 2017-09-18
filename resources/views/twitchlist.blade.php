770e58a2e9bb0e47ee93588ef59716099a72929f
Cory Rodriguez <coreyreylp@gmail.com>
2017-09-18 14:14:47 -0500

    Remove thumbnail, add video length

--- resources/views/twitchlist.blade.php
+++ resources/views/twitchlist.blade.php
@@ -3,11 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <title>Twitchlist</title>
-    <style>
-        .thumbnail {
-            width: 2rem;
-        }
-    </style>
 </head>
 <body>
 @foreach ($channels as $name => $videos)
@@ -15,15 +10,12 @@
 <ol>
 @foreach ($videos as $video)
     <li>
-        <a href="{{ $video->url }}">
-        <img src="{{ $video->thumbnails[0]->url }}" class="thumbnail" alt="Thumb">
-        {{ $video->title }}
-        </a>
+        <a href="{{ $video->url }}">{{ $video->title }}</a>
+        &mdash; <time>{{ gmdate("g:i:s", $video->length) }}</time>
     </li>
 @endforeach
 </ol>
 @endforeach
 <footer>By <a href="https://github.com/CoryRay" target="_blank" rel="noopener">Cory Rodriguez</a></footer>
-
 </body>
 </html>
