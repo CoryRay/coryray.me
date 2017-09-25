f43f0d298e96817ae61d19052b72d7651d96a0fc
Cory Rodriguez <coreyreylp@gmail.com>
2017-09-25 13:43:12 -0500

    Update home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,28 +66,24 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+            {{-- @if (Route::has('login')) --}}
                 <div class="top-right links">
-                    @auth
+                    {{-- @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
-                    @endauth
+                    @endauth --}}
+                    <div>By <a href="https://github.com/CoryRay" target="_blank" rel="noopener">Cory Rodriguez</a></div>
                 </div>
-            @endif
+            {{-- @endif --}}
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                <div class="title m-b-md">My Things</div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ url('/twitchlist') }}">Twitchlist</a>
+                    <a href="{{ url('/calc') }}">Cost Per Unit Calculator</a>
                 </div>
             </div>
         </div>
